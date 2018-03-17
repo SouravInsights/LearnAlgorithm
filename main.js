0b92087d8e1dbfbf26dbb4e0ceee71c5eaf53dcd
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-03-17 10:15:47 +0530

    Updated

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $( "#cell11" ).click(function() {
-  alert( "New Features Coming Soon" );
+  $( "#cell3, #cell10, #cell12, #cell19").css("background-color", "#00e500");
 });
 
+$( "#cell12" ).click(function() {
+   $( "#cell12" ).css("background-color", "#e6e600");
+   $( "#cell4, #cell13, #cell20" ).css("background-color", "#00e500")
+});
