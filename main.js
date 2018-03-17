82a890775fdfb047550fce65b18b598bc8730143
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-03-17 13:07:10 +0530

    Updated

--- main.js
+++ main.js
@@ -2,20 +2,15 @@ $( "#cell11" ).click(function() {
   $( "#cell3, #cell10, #cell12, #cell19").css("background-color", "#00e500");    
 });
 
-$("#cell11").hover(function() {
-        $(this).css('cursor','pointer'); 
-    });
-
 $( "#cell12" ).click(function() {
    $( "#cell12" ).css("background-color", "#e6e600");
    $( "#cell4, #cell13, #cell20" ).css("background-color", "#00e500")
 });
+ 
 
 $( "#cell20" ).click(function() {
      $( "#cell20" ).css("background-color", "#e6e600");
-     $( "#cell21, #cell28" ).css("background-color", "#00e500")
-    
-     
+     $( "#cell21, #cell28" ).css("background-color", "#00e500")     
 });
 
 $( "#cell28" ).click(function() {
@@ -41,4 +36,45 @@ $( "#cell25" ).click(function() {
 $( "#cell33" ).click(function() {
      $( "#cell33" ).css("background-color", "#e6e600");
      $( "#cell41" ).css("background-color", "#00e500")
+});
+
+$( "#cell41" ).click(function() {
+     $( "#cell41" ).css("background-color", "#e6e600");
+     $( "#cell49, #cell42" ).css("background-color", "#00e500")
+});
+
+$( "#cell49" ).click(function() {
+     $( "#cell49" ).css("background-color", "#e6e600");
+     $( "#cell57, #cell50" ).css("background-color", "#00e500")
+});
+
+$( "#cell50" ).click(function() {
+     $( "#cell50" ).css("background-color", "#e6e600");
+     $( "#cell51, #cell58" ).css("background-color", "#00e500")
+});
+
+$( "#cell51" ).click(function() {
+     $( "#cell51" ).css("background-color", "#e6e600");
+     $( "#cell43, #cell52, #cell59" ).css("background-color", "#00e500")
+});
+
+$( "#cell52" ).click(function() {
+     $( "#cell52" ).css("background-color", "#e6e600");
+     $( "#cell53, #cell44, #cell60" ).css("background-color", "#00e500")
+});
+
+$( "#cell44" ).click(function() {
+     $( "#cell44" ).css("background-color", "#e6e600");
+     $( "#cell37, #cell45" ).css("background-color", "#00e500")
+});
+
+$( "#cell45" ).click(function() {
+     $( "#cell45" ).css("background-color", "#e6e600");
+     $( "#cell46" ).css("background-color", "#00e500")
+});
+
+$( "#cell46" ).click(function() {
+     $( "#cell46" ).css("background-color", "#e6e600");
+     alert("You Win")
+     $("#cell47").animate({backgroundColor:'#000080'});
 });
