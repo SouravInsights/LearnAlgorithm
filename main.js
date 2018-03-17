229a44f9c94b6cbdb1d8c203ce4dd90dc74e2fb5
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-03-17 10:54:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ $( "#cell12" ).click(function() {
    $( "#cell12" ).css("background-color", "#e6e600");
    $( "#cell4, #cell13, #cell20" ).css("background-color", "#00e500")
 });
+
+$( "#cell20" ).click(function() {
+     $( "#cell20" ).css("background-color", "#e6e600");
+     $( "#cell21, #cell28" ).css("background-color", "#00e500")
+});
+
+$( "#cell28" ).click(function() {
+     $( "#cell28" ).css("background-color", "#e6e600");
+     $( "#cell27, #cell29 ,#cell36" ).css("background-color", "#00e500")
+});
+
+$( "#cell27" ).click(function() {
+     $( "#cell27" ).css("background-color", "#e6e600");
+     $( "#cell26, #cell35" ).css("background-color", "#00e500")
+});
