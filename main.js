b7c4b4beda43b9ee5fbe8508ca03921b16addabd
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-03-17 11:57:14 +0530

    Things are now Updated.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $( "#cell11" ).click(function() {
-  $( "#cell3, #cell10, #cell12, #cell19").css("background-color", "#00e500");
+  $( "#cell3, #cell10, #cell12, #cell19").css("background-color", "#00e500");    
 });
+
+$("#cell11").hover(function() {
+        $(this).css('cursor','pointer'); 
+    });
 
 $( "#cell12" ).click(function() {
    $( "#cell12" ).css("background-color", "#e6e600");
@@ -10,6 +14,8 @@ $( "#cell12" ).click(function() {
 $( "#cell20" ).click(function() {
      $( "#cell20" ).css("background-color", "#e6e600");
      $( "#cell21, #cell28" ).css("background-color", "#00e500")
+    
+     
 });
 
 $( "#cell28" ).click(function() {
@@ -20,4 +26,19 @@ $( "#cell28" ).click(function() {
 $( "#cell27" ).click(function() {
      $( "#cell27" ).css("background-color", "#e6e600");
      $( "#cell26, #cell35" ).css("background-color", "#00e500")
+});
+
+$( "#cell26" ).click(function() {
+     $( "#cell26" ).css("background-color", "#e6e600");
+     $( "#cell18, #cell25, #cell34" ).css("background-color", "#00e500")
+});
+
+$( "#cell25" ).click(function() {
+     $( "#cell25" ).css("background-color", "#e6e600");
+     $( "#cell17, #cell33" ).css("background-color", "#00e500")
+});
+
+$( "#cell33" ).click(function() {
+     $( "#cell33" ).css("background-color", "#e6e600");
+     $( "#cell41" ).css("background-color", "#00e500")
 });
