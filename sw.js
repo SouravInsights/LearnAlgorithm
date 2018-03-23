aa4e0d3402703698aee86d7fef9cd8335f6965be
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-03-23 11:41:51 +0530

    Updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var cacheName = 'LearnAlgo';
 var filesToCache = [
-  './',
+  '/',
   'index.html',
   'style.css',
   'main.js'
@@ -10,7 +10,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+      return cache.addAll('filesToCache');
     })
   );
 });
