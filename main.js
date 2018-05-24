b2f8059dc780dbe8bae0e5a0fd02e3ff23984c61
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-05-25 01:54:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -45,3 +45,11 @@ function addListener(){
 }
 
 window.onload = addListener;
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
