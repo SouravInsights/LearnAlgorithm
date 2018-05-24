1d1979be354bd33d96ddd039bc9b8ecc3a7a7d92
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2018-05-25 01:56:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,16 @@ window.onload = addListener;
 function displayNotification() {
   if (Notification.permission == 'granted') {
     navigator.serviceWorker.getRegistration().then(function(reg) {
-      reg.showNotification('Hello world!');
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification('Hello world!', options);
     });
   }
 }
